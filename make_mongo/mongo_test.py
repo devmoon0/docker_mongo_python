5fdefecd2b1544b1f2aaa9c49049de22f7975a01
devmoon0 <jmoon9529@gmail.com>
2021-02-01 17:54:34 +0900

    remove trash

--- make_mongo/mongo_test.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-uri = "mongodb://{}:{}@{}:{}/{}".format("root", "root", 'localhost', int(26016), "test_db")
-instanceConnection = MongoClient(uri,connect=False)
-dbs = instanceConnection.list_database_names()
- 
-print(dbs)
